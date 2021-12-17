6242ada473783b00ec5555ccd50301cbf19217d8
princegobah <babynamek@gmail.com>
2021-12-16 21:59:01 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solutions in this file
+let names=[]
+function writeCards(gh,io){
+    for (let i=0;i<gh.length;i++)
+     names.push(`Thank you, ${gh[i]}, for the wonderful ${io} gift!`);return names}
+function countDown(number){while (number>-1) {console.log(number--)}}
+//console.log(writeCards(['prince','sam','alex']))
